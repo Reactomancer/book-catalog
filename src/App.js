e2ffe8f80630c054475e57b713d51ed09629131f
equsix <omar.m.zaid@hotmail.com>
2023-05-31 17:22:08 +0500

    chore fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
 import {
   getFirestore,
   collection,
@@ -6,18 +7,21 @@ import {
   addDoc,
   deleteDoc,
   doc,
+  updateDoc,
 } from "firebase/firestore";
-import { db } from "./firebase";
 
 function App() {
   const [books, setBooks] = useState([]);
   const [newBook, setNewBook] = useState({
+    id: "",
     title: "",
     authors: [],
     year: "",
     rating: "",
     isbn: "",
   });
+  const [groupMode, setGroupMode] = useState("year");
+
   const bookCardStyles = {
     border: "1px solid #ccc",
     borderRadius: "8px",
@@ -58,8 +62,22 @@ function App() {
       return;
     }
 
+    if (newBook.isbn && !validateISBN(newBook.isbn)) {
+      console.error("Invalid ISBN");
+      return;
+    }
+
     try {
-      const docRef = await addDoc(collection(db, "books"), newBook);
+      const { title, authors, year, rating, isbn } = newBook;
+      const bookToAdd = {
+        title,
+        authors: authors.split(",").map((author) => author.trim()),
+        year,
+        rating,
+        isbn,
+      };
+
+      const docRef = await addDoc(collection(db, "books"), bookToAdd);
       setNewBook({
         title: "",
         authors: [],
@@ -81,6 +99,53 @@ function App() {
       console.log("Book deleted with ID:", id);
     } catch (error) {
       console.error("Error deleting book:", error);
+    }
+  };
+
+  const editBook = async (book) => {
+    setNewBook({ ...book });
+  };
+
+  const updateBook = async (id) => {
+    try {
+      await updateDoc(doc(db, "books", id), newBook);
+      setNewBook({
+        id: "",
+        title: "",
+        authors: [],
+        year: "",
+        rating: "",
+        isbn: "",
+      });
+      fetchBooks();
+      console.log("Book updated with ID:", id);
+    } catch (error) {
+      console.error("Error updating book:", error);
+    }
+  };
+
+  const cancelEdit = () => {
+    setNewBook({
+      id: "",
+      title: "",
+      authors: [],
+      year: "",
+      rating: "",
+      isbn: "",
+    });
+  };
+
+  const validateISBN = (isbn) => {
+    let regex = new RegExp(
+      /^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$/
+    );
+    if (isbn == null) {
+      return "false";
+    }
+    if (regex.test(isbn) === true) {
+      return "true";
+    } else {
+      return "false";
     }
   };
 
@@ -119,19 +184,34 @@ function App() {
   };
 
   const groupAndSortBooks = (books) => {
-    const sortedBooks = [...books].sort(
-      (a, b) => b.year - a.year || b.title.localeCompare(a.title)
-    );
+    let sortedBooks = [...books];
+
+    if (groupMode === "rating") {
+      sortedBooks = sortedBooks.filter((book) => book.rating !== undefined);
+      sortedBooks.sort((a, b) => b.rating - a.rating);
+    } else if (groupMode === "author") {
+      sortedBooks.sort((a, b) => a.authors[0].localeCompare(b.authors[0]));
+    } else if (groupMode === "year") {
+      sortedBooks.sort((a, b) => b.year - a.year);
+    }
+
     const groups = {};
     sortedBooks.forEach((book) => {
-      const groupYear = book.year || "No Year";
-      if (!groups[groupYear]) {
-        groups[groupYear] = [];
+      const groupKey = book[groupMode] || "No Value";
+      if (!groups[groupKey]) {
+        groups[groupKey] = [];
       }
-      groups[groupYear].push(book);
+      groups[groupKey].push(book);
     });
     return groups;
   };
+  const changeGroupingMode = (mode) => {
+    setGroupMode(mode);
+  };
+
+  useEffect(() => {
+    changeGroupingMode("year");
+  }, []);
 
   const bookGroups = groupAndSortBooks(books);
   const recommendedBook = getRecommendedBook();
@@ -186,17 +266,27 @@ function App() {
         <div style={bookCardStyles}>
           <h4>{recommendedBook.title}</h4>
           <p>Authors: {recommendedBook.authors}</p>
-          <p>ISBN: {recommendedBook.isbn}</p>
-          <p>Rating: {recommendedBook.rating}</p>
+          {recommendedBook.isbn && <p>ISBN: {recommendedBook.isbn}</p>}
+          {recommendedBook.rating && <p>Rating: {recommendedBook.rating}</p>}
         </div>
       ) : (
         <p>No recommended book found.</p>
       )}
 
       <h2>All Books</h2>
-      {Object.entries(bookGroups).map(([year, books]) => (
-        <div key={year}>
-          <h3>{year}</h3>
+      <div>
+        <select
+          value={groupMode}
+          onChange={(e) => changeGroupingMode(e.target.value)}
+        >
+          <option value="year">Year</option>
+          <option value="rating">Rating</option>
+          <option value="author">Author</option>
+        </select>
+      </div>
+      {Object.entries(bookGroups).map(([groupKey, books]) => (
+        <div key={groupKey}>
+          <h3>{groupKey}</h3>
           {books.map((book) => (
             <div key={book.id} style={bookCardStyles}>
               <h4>{book.title}</h4>
@@ -204,10 +294,56 @@ function App() {
               {book.isbn && <p>ISBN: {book.isbn}</p>}
               {book.rating && <p>Rating: {book.rating}</p>}
               <button onClick={() => deleteBook(book.id)}>Delete</button>
+              <button onClick={() => editBook(book)}>Edit</button>
             </div>
           ))}
         </div>
       ))}
+      {newBook.id && (
+        <div>
+          <h3>Edit Book</h3>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={newBook.title}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="authors"
+            placeholder="Authors (comma-separated)"
+            value={newBook.authors}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="year"
+            placeholder="Year (optional)"
+            min="1800"
+            value={newBook.year}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="rating"
+            placeholder="Rating (0-10)"
+            min="0"
+            max="10"
+            value={newBook.rating}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="isbn"
+            placeholder="ISBN (optional)"
+            value={newBook.isbn}
+            onChange={handleInputChange}
+          />
+          <button onClick={() => updateBook(newBook.id)}>Save</button>
+          <button onClick={cancelEdit}>Cancel</button>
+        </div>
+      )}
     </div>
   );
 }
